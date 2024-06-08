87f9959fe82dfe6edebefadc33c7b5d846d3444e
Sebastiao Mesquita <sebastiaokmequita@msn.com>
2024-06-08 22:08:21 +0200

    pre-entrega 2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // script.js
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.menu-toggle').addEventListener('click', function() {
-        document.querySelector('.menu-items').classList.toggle('active');
-    });
-});
+const myfunction = () => {
+    console.log("click")
+    document.querySelector('.menu-items-desktop').classList.toggle('active');
+}
+    
