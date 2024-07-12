9de2200b86b88c6852f7efcf1542738f778c389b
Sebastiao Mesquita <sebastiaokmequita@msn.com>
2024-07-12 19:54:32 +0200

    new carousel

--- js/script.js
+++ js/script.js
@@ -3,4 +3,22 @@ const myfunction = () => {
     console.log("click")
     document.querySelector('.menu-items-desktop').classList.toggle('active');
 }
-    
+
+let slideIndex = 1;
+showSlides(slideIndex);
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+ 
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("carousel-slide");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slides[slideIndex-1].style.display = "block";  
+}
+
+
